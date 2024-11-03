790bc78e6e98a5db9546bfda2c6c69d4425bf2d5
engels <engels.franca@icloud.com>
2024-11-02 23:54:27 -0300

    first commit

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend "s3" {
+      bucket = "estudosterraform-engels"
+      key = "terraform-testes"
+      region = "us-east-2"
+    }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
